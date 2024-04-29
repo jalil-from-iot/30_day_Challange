21bc20e7673d1a83ff59dd086ec62367f53e8a0d
jalil-from-iot <148869103+jalil-from-iot@users.noreply.github.com>
2024-04-29 22:02:48 +0500

    Add files via upload

---
+++ day5/num_evaluate.py
@@ -0,0 +1,8 @@
+num = int(input('Enter a number\n'))
+
+if num > 0:
+    print(f'Number {num} is positive')
+elif num < 0:
+    print(f'Number {num} is negative')
+else:
+    print(f'Number {num} is zero')
